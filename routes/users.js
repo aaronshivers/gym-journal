4c28ce725a6b7f7dc323aa5b845293a3c391ac7d
aaronshivers <aaron@moonmail.org>
2019-05-01 20:02:53 -0500

    added tests and route for POST /users/login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 const User = require('../models/users')
 const userValidator = require('../middleware/userValidator')
@@ -38,6 +39,43 @@ router.post('/users', validate(userValidator), async (req, res) => {
 
   }
 
+})
+
+// POST /users/login
+router.post('/users/login', async (req, res) => {
+
+  try {
+    
+    // get email and password
+    const { email, password } = req.body
+
+    // find user by email
+    const user = await User.findOne({ email })
+
+    // reject if user is not found
+    if (!user) return res.status(401).render('error', { msg: 'User Not Found' })
+
+    // verify user password
+    const hash = await bcrypt.compare(password, user.password)
+
+    // reject if password is incorrect
+    if (!hash) return res.status(401).render('error', { msg: 'Please check your login credentials, and try again.' })
+
+    // create token
+    const token = await user.createAuthToken()
+
+    // set cookie options
+    const cookieOptions = { expires: new Date(Date.now() + 86400000) }
+
+    // set cookie and redirect to /users/profile
+    res.cookie('token', token, cookieOptions).status(200).redirect(`/users/me`)
+
+  } catch (error) {
+
+  // send error message
+  res.render('error', { msg: error.message })
+
+  }
 })
 
 module.exports = router
